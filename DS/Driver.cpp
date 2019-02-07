7f7cdc1416603af08f0a9fb57525f5e414c92b1d
SharmaMohit <mohit.sharma@tatahealth.com>
2019-02-07 17:08:10 +0530

    Data Structure

--- DS/Driver.cpp
+++ DS/Driver.cpp
@@ -1,10 +1,42 @@
 #include <bits/stdc++.h>
 // #include "SinglyLinkedList.h"
 // #include "DoublyLinkedList.h"
+#include "CircularLinkedList.h"
 
 using namespace std;
 
 int main()
 {
-    
+    CircularLinkedList<int> cll;
+    try
+    {
+        cll.PushFront(2);
+        cll.PrintList();
+        cll.PushFront(1);
+        cll.PrintList();
+        cll.Insert(5, 3);
+        cll.Insert(3, 3);
+        cll.Insert(4, 4);
+        cll.PrintList();
+        cll.PopFront();
+        cll.PrintList();
+        cll.PopBack();
+        cll.PrintList();
+        cll.PopFront();
+        cll.PrintList();
+        cll.PopBack();
+        cll.PrintList();
+        cll.PopFront();
+        cll.PrintList();
+        cll.PopBack();
+        cll.PrintList();
+    }
+    catch (const char *e)
+    {
+        cout << e << '\n';
+    }
+    catch (...)
+    {
+        cout << "Unknown Exception";
+    }
 }
