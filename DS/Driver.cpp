459135b61580ebe986c4d4d0337a837835801c10
SharmaMohit <mohit.sharma@tatahealth.com>
2019-02-08 16:43:56 +0530

    Implemented BT, BST, Heap

--- DS/Driver.cpp
+++ DS/Driver.cpp
@@ -1,30 +1,19 @@
 #include <bits/stdc++.h>
+using namespace std;
+
 // #include "SinglyLinkedList.h"
 // #include "DoublyLinkedList.h"
 // #include "CircularLinkedList.h"
 // #include "ArrayStack.h"
-#include "LLStack.h"
-using namespace std;
+// #include "LLStack.h"
+// #include "PriorityQueue.h"
+// #include "BinaryTree.h"
+// #include "BinarySearchTree.h"
 
 int main()
 {
     try
     {
-        LLStack<int> S;
-        S.Push(1);
-        S.Push(2);
-        S.Push(3);
-        S.Push(4);
-        S.Push(5);
-        S.Push(6);
-        S.Push(7);
-        S.Push(8);
-        cout << S.Size() << "\n";
-        while (!S.Empty())
-        {
-            cout << S.Top() << " ";
-            S.Pop();
-        }
     }
     catch (const char *excep)
     {
