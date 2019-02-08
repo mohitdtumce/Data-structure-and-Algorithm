4b94502bbcfe0dad2caeacd0f4aaf99c4a127852
SharmaMohit <mohit.sharma@tatahealth.com>
2019-02-08 10:56:57 +0530

    Stack Implementation - Array and LinkedList

--- DS/Driver.cpp
+++ DS/Driver.cpp
@@ -1,42 +1,33 @@
 #include <bits/stdc++.h>
 // #include "SinglyLinkedList.h"
 // #include "DoublyLinkedList.h"
-#include "CircularLinkedList.h"
-
+// #include "CircularLinkedList.h"
+// #include "ArrayStack.h"
+#include "LLStack.h"
 using namespace std;
 
 int main()
 {
-    CircularLinkedList<int> cll;
     try
     {
-        cll.PushFront(2);
-        cll.PrintList();
-        cll.PushFront(1);
-        cll.PrintList();
-        cll.Insert(5, 3);
-        cll.Insert(3, 3);
-        cll.Insert(4, 4);
-        cll.PrintList();
-        cll.PopFront();
-        cll.PrintList();
-        cll.PopBack();
-        cll.PrintList();
-        cll.PopFront();
-        cll.PrintList();
-        cll.PopBack();
-        cll.PrintList();
-        cll.PopFront();
-        cll.PrintList();
-        cll.PopBack();
-        cll.PrintList();
+        LLStack<int> S;
+        S.Push(1);
+        S.Push(2);
+        S.Push(3);
+        S.Push(4);
+        S.Push(5);
+        S.Push(6);
+        S.Push(7);
+        S.Push(8);
+        cout << S.Size() << "\n";
+        while (!S.Empty())
+        {
+            cout << S.Top() << " ";
+            S.Pop();
+        }
     }
-    catch (const char *e)
+    catch (const char *excep)
     {
-        cout << e << '\n';
-    }
-    catch (...)
-    {
-        cout << "Unknown Exception";
+        cout << excep << '\n';
     }
 }
